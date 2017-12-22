d1a781f2433271cf1fcd7058dc38d6cb40b145bd
vinarut <arut.73@gmail.com>
2017-12-22 22:11:31 +0300

    Рефакторинг authorization.js registration.js socket.js

--- www/js/authorization.js
+++ www/js/authorization.js
@@ -2,11 +2,8 @@ var bestscore, Coins, time;
 
 $("#btnEnter").on('click', (function() {
 
-  var nick = $('#inputNickname').val(),
-      pass = $('#inputPassword').val();
-
-  nick.trim();
-  pass.trim();
+  var nick = $('#inputNickname').val().trim(),
+      pass = $('#inputPassword').val().trim();
 
   if (nick.length == 0 || pass.length == 0) {
     document.getElementById('validationAuth').style.display = 'block';
@@ -17,64 +14,70 @@ $("#btnEnter").on('click', (function() {
   }
 
   socket.on('auth', function (data) {
-    if (data.successfully) {
-      $('#overlay-start').fadeOut(50);
-      $('#mainScreen').fadeIn(50);
-      document.getElementById('mainScreenElements').style.visibility = "visible";
 
-      bestscore = data.bestscore;
-      Coins = data.coins;
-      time = data.time;
-      $('#mainScreenStatistic p:nth-child(1)').text('Nickname: ' + nick);
-      $('#mainScreenStatistic p:nth-child(2)').text('Best score: ' + bestscore);
-      $('#mainScreenStatistic p:nth-child(3)').text('Coins: ' + Coins);
-      var hour = Math.floor(time / 3600000),
-          minute = Math.floor((time % 3600000) / 60000),
-          second = Math.floor(((time % 3600000) % 60000) / 1000);
-          $('#mainScreenStatistic p:nth-child(4)').text('Total time: ' + hour + 'h ' + minute + 'm ' + second + 's');
-          // console.log('Auth: ' + Coins);
+    if (!data.successfully) {
+      document.getElementById('validationAuth').style.display = 'block';
+      document.getElementById('validationAuth').innerHTML = 'Authorization failed! Please enter correct data';
+      $('#validationAuth').addClass('error');
 
-          data.topscores.forEach(function (item, i) {
-            $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(1)').text(i + 1);
-            $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(2)').text(item.username);
-            $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(3)').text(item.score);
-          });
-
-          socket.on('update-top-score', function (data) {
-
-            data.scores.forEach(function (item, i) {
-              $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(1)').text(i + 1);
-              $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(2)').text(item.username);
-              $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(3)').text(item.score);
-            });
-
-          })
-          .on('update-online', function (data) {
-            $('#mainScreenOnline p').text('Online: ' + data.pOnline);
-          })
-          .on('player-data', function (data) {
-            $('#mainScreenStatistic p:nth-child(2)').text('Best score: ' + data.bestscore);
-            $('#mainScreenStatistic p:nth-child(3)').text('Coins: ' + data.coins);
-            hour = Math.floor(data.time / 3600000),
-            minute = Math.floor((data.time % 3600000) / 60000),
-            second = Math.floor(((data.time % 3600000) % 60000) / 1000);
-            $('#mainScreenStatistic p:nth-child(4)').text('Total time: ' + hour + 'h ' + minute + 'm ' + second + 's'); 
-
-            Coins = data.coins;
-                // console.log('player-data: ' + Coins);
-            if (Coins < continueFor) {
-              document.getElementById('btn-continue').disabled = true;
-              document.getElementById('btn-continue').title = 'You need more coins';
-            } else {
-                $('#btn-continue').removeAttr('title');
-            }
-          });
-
-    } else {
-        document.getElementById('validationAuth').style.display = 'block';
-        document.getElementById('validationAuth').innerHTML = 'Authorization failed! Please enter correct data';
-        $('#validationAuth').addClass('error');
+      return;
     }
+
+    $('#overlay-start').fadeOut(50);
+    $('#mainScreen').fadeIn(50);
+    document.getElementById('mainScreenElements').style.visibility = "visible";
+
+    bestscore = data.bestscore;
+    Coins = data.coins;
+    time = data.time;
+    
+    $('#mainScreenStatistic p:nth-child(1)').text('Nickname: ' + nick);
+    $('#mainScreenStatistic p:nth-child(2)').text('Best score: ' + bestscore);
+    $('#mainScreenStatistic p:nth-child(3)').text('Coins: ' + Coins);
+
+    var hour = Math.floor(time / 3600000),
+        minute = Math.floor((time % 3600000) / 60000),
+        second = Math.floor(((time % 3600000) % 60000) / 1000);
+
+    $('#mainScreenStatistic p:nth-child(4)').text('Total time: ' + hour + 'h ' + minute + 'm ' + second + 's');
+
+    data.topscores.forEach(function (item, i) {
+      $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(1)').text(i + 1);
+      $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(2)').text(item.username);
+      $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(3)').text(item.score);
+    });
+
+    socket.on('update-top-score', function (data) {
+
+      data.scores.forEach(function (item, i) {
+        $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(1)').text(i + 1);
+        $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(2)').text(item.username);
+        $('#mainScreenTop > tbody > tr:nth-child(' + (i + 1) + ') > td:nth-child(3)').text(item.score);
+      });
+
+    })
+    .on('update-online', function (data) {
+      $('#mainScreenOnline p').text('Online: ' + data.pOnline);
+    })
+    .on('player-data', function (data) {
+      $('#mainScreenStatistic p:nth-child(2)').text('Best score: ' + data.bestscore);
+      $('#mainScreenStatistic p:nth-child(3)').text('Coins: ' + data.coins);
+
+      hour = Math.floor(data.time / 3600000),
+      minute = Math.floor((data.time % 3600000) / 60000),
+      second = Math.floor(((data.time % 3600000) % 60000) / 1000);
+
+      $('#mainScreenStatistic p:nth-child(4)').text('Total time: ' + hour + 'h ' + minute + 'm ' + second + 's'); 
+
+      Coins = data.coins;
+
+      if (Coins < continueFor) {
+        document.getElementById('btn-continue').disabled = true;
+        document.getElementById('btn-continue').title = 'You need more coins';
+      } else {
+          $('#btn-continue').removeAttr('title');
+      }
+    });
   });
 }));
 
--- www/js/registration.js
+++ www/js/registration.js
@@ -4,55 +4,63 @@ $('#btnReg').on('click', function () {
 
 $('#btnRegRegistr').on('click', function () {
 
-  var nick = $('#inputNick').val(),
-      pass = $('#inputPass').val(),
-      repeatPass = $('#inputRepeatPass').val();
-
-  nick.trim();
-  pass.trim();
-  repeatPass.trim();
+  var nick = $('#inputNick').val().trim(),
+      pass = $('#inputPass').val().trim(),
+      repeatPass = $('#inputRepeatPass').val().trim();
 
   if (nick.length == 0 || pass.length == 0 || repeatPass.length == 0) {
     document.getElementById('validationReg').style.display = 'block';
     document.getElementById('validationReg').innerHTML = 'Fields must not be empty';
     $('#validationReg').addClass('error');
-  } else {
-      if (nick.length < 3 || nick.length > 15) {
-        document.getElementById('validationReg').style.display = 'block';
-        document.getElementById('validationReg').innerHTML = 'Nickname must be more than 3 and less than 15 characters';
-        $('#validationReg').addClass('error');
-      } else {
-          if (pass.length < 5) {
-            document.getElementById('validationReg').style.display = 'block';
-            document.getElementById('validationReg').innerHTML = 'Password must be at least 5 characters';
-            $('#validationReg').addClass('error');
-          } else {
-              if (pass != repeatPass) {
-                document.getElementById('validationReg').style.display = 'block';
-                document.getElementById('validationReg').innerHTML = 'Passwords do not match';
-                $('#validationReg').addClass('error');
-              } else {
-                  socket.emit('registration', { username: nick, password: pass });
-              }
-          }                   
-      }
+
+    return;
   } 
 
+  if (nick.length < 3 || nick.length > 15) {
+    document.getElementById('validationReg').style.display = 'block';
+    document.getElementById('validationReg').innerHTML = 'Nickname must be more than 3 and less than 15 characters';
+    $('#validationReg').addClass('error');
+
+    return;
+  }
+
+  if (pass.length < 5) {
+    document.getElementById('validationReg').style.display = 'block';
+    document.getElementById('validationReg').innerHTML = 'Password must be at least 5 characters';
+    $('#validationReg').addClass('error');
+
+    return;
+  }
+
+  if (pass != repeatPass) {
+    document.getElementById('validationReg').style.display = 'block';
+    document.getElementById('validationReg').innerHTML = 'Passwords do not match';
+    $('#validationReg').addClass('error');
+
+    return;
+  } 
+  
+  socket.emit('registration', { username: nick, password: pass }); 
+
   socket.on('registration', function (data) {
-    if (data.successfully) {
+
+    if (!data.successfully) {
       document.getElementById('validationReg').style.display = 'block';
-      document.getElementById('validationReg').innerHTML = 'You are successfully registered';
-        if ($('#validationReg').hasClass('error')) {
-          $('#validationReg').removeClass('error');
-          $('#validationReg').addClass('success');                                    
-        } else {
-            $('#validationReg').addClass('success');
-        }
-    } else {
-        document.getElementById('validationReg').style.display = 'block';
-        document.getElementById('validationReg').innerHTML = 'Registration failed! User with such a nickname already exists';
-        $('#validationReg').addClass('error');  
+      document.getElementById('validationReg').innerHTML = 'Registration failed!';
+      $('#validationReg').addClass('error');
+
+      return; 
     }
+
+    document.getElementById('validationReg').style.display = 'block';
+    document.getElementById('validationReg').innerHTML = 'You are successfully registered';
+
+    if ($('#validationReg').hasClass('error')) {
+      $('#validationReg').removeClass('error');
+    } 
+
+    $('#validationReg').addClass('success'); 
+        
   });
 });
 
